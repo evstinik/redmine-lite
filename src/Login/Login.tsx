3c7806345feffc2753dc09b2fd3dc9169504fabd
Nikita Evstigneev <nikita.evstigneev@sabo-gmbh.de>
2020-07-27 20:03:45 +0200

    Added also easiest login screen design

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { useRedmineService } from '../hooks/redmineService'
 import { useAppState } from '../hooks/appState'
 import { useOnChange } from '../hooks/utils'
+import './Login.css'
 
 export function Login() {
   const [apiKey, setApiKey] = React.useState('')
@@ -26,14 +27,14 @@ export function Login() {
   }, [redmineService, apiKey, setAppState])
 
   return (
-    <div>
+    <div className="login page">
       <h1>Please login</h1>
       <form onSubmit={login}>
         <label>API Key</label>
         <input value={apiKey} onChange={handleInputChange} required />
-        <input type='submit' value='Login'></input>
+        <input type='submit' value='Login' className='contained'></input>
       </form>
-      <div>
+      <div className="tip">
         You can find your API key on your account page ( /my/account ) when
         logged in, on the right-hand pane of the default layout.
       </div>
