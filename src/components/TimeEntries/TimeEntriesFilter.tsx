085df0fc93542c32fc3310f3650f899fd644e2e3
Nikita Evstigneev <nikita.evstigneev@saboit.de>
2023-07-25 20:07:52 +0200

    Locale-based calendar.

--- src/components/TimeEntries/TimeEntriesFilter.tsx
+++ src/components/TimeEntries/TimeEntriesFilter.tsx
@@ -3,8 +3,13 @@ import DatePicker, { registerLocale } from 'react-datepicker'
 import { useAppState } from 'hooks/appState'
 import './TimeEntriesFilter.css'
 import 'react-datepicker/dist/react-datepicker.css'
+
 import cs from 'date-fns/locale/cs'
+import en from 'date-fns/locale/en-GB'
+import de from 'date-fns/locale/de'
 registerLocale('cs', cs)
+registerLocale('en', en)
+registerLocale('de', de)
 
 export function TimeEntriesFilter() {
   const [{ dayForTimeEntries = new Date() }, setAppState] = useAppState()
@@ -30,6 +35,7 @@ export function TimeEntriesFilter() {
             dateFormat='dd.MM.yyyy'
             selected={dayForTimeEntries}
             onChange={setDate}
+            locale={navigator.language.slice(0, 2)}
           />
         </label>
       </form>
