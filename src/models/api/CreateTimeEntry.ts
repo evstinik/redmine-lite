c978ee0fa3fb16f61ea0b1cb86336da1bbbbd33e
Radmir Gurkov <ragux0@gmail.com>
2024-10-11 19:15:31 +0200

    Add additional format support

--- src/models/api/CreateTimeEntry.ts
+++ src/models/api/CreateTimeEntry.ts
@@ -1,6 +1,6 @@
 export interface CreateTimeEntry {
   issue_id: number
-  hours: number
+  hours: number | string // format can be in 1h20m for example https://www.redmine.org/projects/redmine/wiki/RedmineTimeTracking
   activity_id: number
   spent_on?: string
   comments: string
