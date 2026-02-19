d6cef363fe53a19dedf71a3d73966e19c3e68c44
Penko, Boris (EXTERN: SMIT GmbH) <boris.penko@saboit.de>
2026-02-19 11:11:10 +0100

    Add create issue and create multiple issues modals to IssuesSearch

--- src/components/Issues/IssuesSearch.tsx
+++ src/components/Issues/IssuesSearch.tsx
@@ -6,6 +6,10 @@ import { Issue } from '@app/models/api/Issue'
 import { useProjects } from '@app/hooks/projects'
 import { AutocompleteSelect } from '@app/components/AutocompleteSelect'
 import TextField from '@mui/material/TextField'
+import MuiIconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import { CreateIssueModal } from './CreateIssueModal'
+import { CreateMultipleIssuesModal } from './CreateMultipleIssuesModal'
 
 import './IssuesSearch.css'
 
@@ -22,8 +26,12 @@ export function IssuesSearch(props: IssuesProps) {
   const [latestQuery, setLatestQuery] = React.useState(query)
   const [latestProjectId, setLatestProjectId] = React.useState<string>(projectId)
 
+  const [createModalOpen, setCreateModalOpen] = React.useState(false)
+  const [createMultipleModalOpen, setCreateMultipleModalOpen] = React.useState(false)
+  const [refreshKey, setRefreshKey] = React.useState(0)
+
   const projects = useProjects() ?? []
-  const [issues, isLoading] = useIssuesSearch(latestQuery, Number(latestProjectId))
+  const [issues, isLoading] = useIssuesSearch(latestQuery, Number(latestProjectId), refreshKey)
 
   const search = React.useCallback(
     (event) => {
@@ -33,6 +41,25 @@ export function IssuesSearch(props: IssuesProps) {
     },
     [projectId, query]
   )
+
+  const handleIssueCreated = React.useCallback(() => {
+    setCreateModalOpen(false)
+    setRefreshKey((k) => k + 1)
+  }, [])
+
+  const handleMultipleIssuesCreated = React.useCallback(() => {
+    setCreateMultipleModalOpen(false)
+    setRefreshKey((k) => k + 1)
+  }, [])
+
+  const handleCloseModal = React.useCallback(() => setCreateModalOpen(false), [])
+  const handleCloseMultipleModal = React.useCallback(() => setCreateMultipleModalOpen(false), [])
+
+  const handleOpenModal = React.useCallback(() => setCreateModalOpen(true), [])
+  const handleOpenMultipleModal = React.useCallback(() => setCreateMultipleModalOpen(true), [])
+
+  const selectedProjectId = Number(projectId)
+  const showCreateIssue = selectedProjectId > 0
 
   return (
     <div className='issues-search'>
@@ -54,6 +81,42 @@ export function IssuesSearch(props: IssuesProps) {
           value={projectId}
           onValueChanged={(pid) => setProjectId(pid ?? '0')}
         />
+        {showCreateIssue && (
+          <Tooltip title='Create new issue'>
+            <MuiIconButton
+              size='small'
+              onClick={handleOpenModal}
+              sx={{
+                ml: 1,
+                color: '#fff',
+                backgroundColor: 'primary.main',
+                width: 32,
+                height: 32,
+                fontSize: '1.25rem',
+                '&:hover': { backgroundColor: 'primary.dark' }
+              }}
+            >
+              +
+            </MuiIconButton>
+          </Tooltip>
+        )}
+        <Tooltip title='Create multiple issues'>
+          <MuiIconButton
+            size='small'
+            onClick={handleOpenMultipleModal}
+            sx={{
+              ml: 1,
+              color: '#fff',
+              backgroundColor: 'secondary.main',
+              width: 32,
+              height: 32,
+              fontSize: '0.85rem',
+              '&:hover': { backgroundColor: 'secondary.dark' }
+            }}
+          >
+            ++
+          </MuiIconButton>
+        </Tooltip>
         <TextField
           className='search-form__query'
           label='Search query'
@@ -79,6 +142,20 @@ export function IssuesSearch(props: IssuesProps) {
           )}
         </>
       )}
+      {showCreateIssue && (
+        <CreateIssueModal
+          open={createModalOpen}
+          projectId={selectedProjectId}
+          onClose={handleCloseModal}
+          onCreated={handleIssueCreated}
+        />
+      )}
+      <CreateMultipleIssuesModal
+        open={createMultipleModalOpen}
+        defaultProjectId={selectedProjectId}
+        onClose={handleCloseMultipleModal}
+        onCreated={handleMultipleIssuesCreated}
+      />
     </div>
   )
 }
